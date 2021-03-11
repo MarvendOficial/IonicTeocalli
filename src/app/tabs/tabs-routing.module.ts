3e07fcec640bd053f0b318eb98e59164724bf0f9
Angelmarcat <gen2013.pom.angelmarcat2506@gmail.com>
2021-03-10 22:07:40 -0600

    restaurado

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -23,50 +23,6 @@ const routes: Routes = [
         path: 'login',
         loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
       },
-      {
-        path: 'explore-container',
-        loadChildren: () => import('../explore-container/explore-container.module').then(m => m.ExploreContainerComponentModule)
-      },
-      {
-        path: 'actualizar-perfil',
-        loadChildren: () => import('../pantallas/actualizar-perfil/actualizar-perfil.module').then(m => m.ActualizarPerfilPageModule)
-      },
-      {
-        path: 'contactar-dueno',
-        loadChildren: () => import('../pantallas/contactar-dueno/contactar-dueno.module').then(m => m.ContactarDuenoPageModule)
-      },
-      {
-        path: 'historial-de-pagos',
-        loadChildren: () => import('../pantallas/historial-de-pagos/historial-de-pagos.module').then(m => m.HistorialDePagosPageModule)
-      },
-      {
-        path: 'inicio-sin-hospedaje',
-        loadChildren: () => import('../pantallas/inicio-sin-hospedaje/inicio-sin-hospedaje.module').then(m => m.InicioSinHospedajePageModule)
-      },
-      {
-        path: 'pagar-renta',
-        loadChildren: () => import('../pantallas/pagar-renta/pagar-renta.module').then(m => m.PagarRentaPageModule)
-      },
-      {
-        path: 'registrar',
-        loadChildren: () => import('../pantallas/registrar/registrar.module').then(m => m.RegistrarPageModule)
-      },
-      {
-        path: 'seleccionar-hospedaje',
-        loadChildren: () => import('../pantallas/seleccionar-hospedaje/seleccionar-hospedaje.module').then(m => m.SeleccionarHospedajePageModule)
-      },
-      {
-        path: 'valorar',
-        loadChildren: () => import('../pantallas/valorar/valorar.module').then(m => m.ValorarPageModule)
-      },
-      {
-        path: 'ver-ubicacion',
-        loadChildren: () => import('../pantallas/ver-ubicacion/ver-ubicacion.module').then(m => m.VerUbicacionPageModule)
-      },
-      {
-        path: 'verificar',
-        loadChildren: () => import('../pantallas/verificar/verificar.module').then(m => m.VerificarPageModule)
-      },
 
       {
         path: '',
